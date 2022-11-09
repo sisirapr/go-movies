3a1da9c56282fc210f8f46e70a423a78f02f8467
sisira.pr <sisira.pr@thoughtworks.com>
2022-11-09 16:36:08 +0530

    React router - BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,36 +1,59 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-
-function App() {
+export default function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <h1 className="mt-3">
-          Go Watch a Movie!
-        </h1>
-        <hr className="mb-3"></hr>
-      </div>
-      <div className="row">
-        <div className="col-md-2">
-          <nav>
-            <ul className="list-group">
-              <li className="list-froup-item">
-                <a href="/">Home</a>
-              </li>
-              <li className="list-froup-item">
-                <a href="/movies">Movies</a>
-              </li>
-              <li className="list-froup-item">
-                <a href="/admin">Manage catalogue</a>
-              </li>
-            </ul>
-          </nav>
+    <Router>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt-3">
+            Go Watch a Movie!
+          </h1>
+          <hr className="mb-3"></hr>
         </div>
-        <div className="col-md-10">
-
+        <div className="row">
+          <div className="col-md-2">
+            <nav>
+              <ul className="list-group">
+                <li className="list-froup-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="list-froup-item">
+                  <Link to="/movies">Movies</Link>
+                </li>
+                <li className="list-froup-item">
+                  <Link to="/admin">Manage catalogue</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col-md-10">
+            <Switch>
+              <Route path="/movies">
+                <Movies />
+              </Route>
+              <Route path="/admin">
+                <Admin />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Movies() {
+  return <h2>Movies</h2>
+}
+
+function Admin() {
+  return <h2>Manage catalogue</h2>
+}
