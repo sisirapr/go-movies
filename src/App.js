6170f7181c797ecd464a4876b7e26271f3d37e65
sisira.pr <sisira.pr@thoughtworks.com>
2022-11-10 10:07:41 +0530

    routing to a component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Movies from "./components/Movies";
 
 export default function App() {
   return (
@@ -48,10 +49,6 @@ export default function App() {
 
 function Home() {
   return <h2>Home</h2>
-}
-
-function Movies() {
-  return <h2>Movies</h2>
 }
 
 function Admin() {
