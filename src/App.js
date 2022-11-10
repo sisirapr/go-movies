e023099ee0cc97571c64c49c307b031fd62e7f21
sisira.pr <sisira.pr@thoughtworks.com>
2022-11-10 14:19:11 +0530

    nested routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, useParams } from 'react-router-dom';
 import Movies from "./components/Movies";
 import Home from "./components/Home";
 import Admin from "./components/Admin";
@@ -32,6 +32,9 @@ export default function App() {
           </div>
           <div className="col-md-10">
             <Switch>
+              <Route path="/movies/:id">
+                <Movie/>
+              </Route>
               <Route path="/movies">
                 <Movies />
               </Route>
@@ -47,4 +50,10 @@ export default function App() {
       </div>
     </Router>
   );
+}
+
+function Movie() {
+  let {id} = useParams();
+
+  return <h2>Movie id: {id}</h2>
 }
